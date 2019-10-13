cbda9203f63a4f189c0e1ab73910df2960a193bd
breno-helf <breno.helfstein@gmail.com>
2019-10-13 18:03:34 -0300

    dois pra todo mundo

--- EP2/utils/context.go
+++ EP2/utils/context.go
@@ -44,7 +44,7 @@ func (ctx *Context) AddNode(node string) {
 	ctx.mu.Lock()
 	defer ctx.mu.Unlock()
 	ctx.nodes[node] = true
-	ctx.workLoad[node] = make(chan bool, 4)
+	ctx.workLoad[node] = make(chan bool, 2)
 }
 
 // RemoveNode remove a node from nodes
